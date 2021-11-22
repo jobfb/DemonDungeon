eda8f5763b2f7b779a79d7458ccfdedece805ecd
Job Ferreira Bazzano <jobbjunior@hotmail.com>
2021-11-22 01:01:31 -0300

    Player movement script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private BoxCollider2D boxCollider;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Vector3 moveDelta;//difference between the loaded frame and the next frame(place were we will be)
+
+	private void Start()
+	{
+		boxCollider = GetComponent<BoxCollider2D>();
+	}
+	private void FixedUpdate() //because we are using physics and manual collision detection
+	{
+
+		float x = Input.GetAxisRaw("Horizontal");//its gonna return -1 if "a" or left arrow is pressed and 1 for "d" or right arrow
+		float y = Input.GetAxisRaw("Vertical");//its gonna return -1 if "a" or left arrow is pressed and 1 for "d" or right arrow
+
+		//Reset moveDelta 
+		moveDelta = new Vector3(x, y, 0);
+
+		//Debug.Log(x);
+		//Debug.Log(y);
+
+		//swap sprite direction right and left
+		if (moveDelta.x > 0)
+			transform.localScale = Vector3.one;
+		else if (moveDelta.x < 0)
+			transform.localScale = new Vector3(-1, 1, 1);
+
+
+		//Make the player move
+		transform.Translate(moveDelta * Time.deltaTime);
+
+	}
+
 }
